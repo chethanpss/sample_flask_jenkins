104ebaa4004d54d08b02232b89509f50bc83f03b
temp <temp@123.com>
2024-01-11 17:50:07 +0530

    new commit

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def hello_world():
    return 'Hello World..  '
 
 
+@app.route('/new_route')
+def new_route():
+   return 'This is a new route..  '
+
+
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0',port=5000)
